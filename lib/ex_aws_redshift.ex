2259e7e1ca76e38daca8738bb58333708b4fef22
May Pongpitpitak <mayppong@ShadowStalker.local>
2017-10-16 13:53:16 -0700

    Initial generated project

---
+++ lib/ex_aws_redshift.ex
@@ -0,0 +1,18 @@
+defmodule ExAwsRedshift do
+  @moduledoc """
+  Documentation for ExAwsRedshift.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAwsRedshift.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aws_redshift_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAwsRedshiftTest do
+  use ExUnit.Case
+  doctest ExAwsRedshift
+
+  test "greets the world" do
+    assert ExAwsRedshift.hello() == :world
+  end
+end
