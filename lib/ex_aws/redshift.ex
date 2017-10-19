7664333fa93159b74b2669bb473044c1315c6aea
May Pongpitpitak <mayppong@ShadowStalker.local>
2017-10-19 14:37:59 -0700

    Switch to use snake_case for keys
    As per `ex_aws` contributing guide, the keys should be snake_case to
    keep it close to Elixir's style instead of following AWS keys.
    The enumerable keys are supported by the keys are not simplified *yet*.
    That will be coming soon.

--- lib/ex_aws/redshift.ex
+++ lib/ex_aws/redshift.ex
@@ -3,38 +3,40 @@ defmodule ExAws.RedShift do
   Operations on AWS RedShift.
   """
 
+  import ExAws.Utils, only: [camelize_key: 1]
+
   @type node_type :: [:ds1_xlarge | :ds1_8xlarge | :ds2_xlarge | :ds2_8xlarge |
                        :dc1_large | :dc1_8xlarge]
 
   @doc """
   Create a new cluster with the given cluster id.
 
-  Required keys are ClusterIdentifier, MasterUsername, MasterUserPassword, and NodeType.
+  Required keys are cluster_identifier, master_username, master_user_password, and node_type.
 
   Amazon docs: http://docs.aws.amazon.com/redshift/latest/APIReference/API_CreateCluster.html
 
   ## Examples
 
-      iex> ExAws.RedShift.create_cluster("A2", [MasterUsername: "Nier", MasterUserPassword: "Automata", NodeType: "dc1_large"])
+      iex> ExAws.RedShift.create_cluster("A2", [master_username: "Nier", master_user_password: "Automata", node_type: "dc1_large"])
       %ExAws.Operation.Query{action: "CreateCluster",
         params: %{"Action" => "CreateCluster", "ClusterIdentifier" => "A2", "MasterUsername" => "Nier", "MasterUserPassword" => "Automata", "NodeType" => "dc1_large"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
 
-      iex> ExAws.RedShift.create_cluster("A2", [ClusterIdentifier: "21O", MasterUsername: "Nier", MasterUserPassword: "Automata", NodeType: "dc1_large"])
+      iex> ExAws.RedShift.create_cluster("A2", [cluster_identifier: "21O", master_username: "Nier", master_user_password: "Automata", node_type: "dc1_large"])
       %ExAws.Operation.Query{action: "CreateCluster",
         params: %{"Action" => "CreateCluster", "ClusterIdentifier" => "A2", "MasterUsername" => "Nier", "MasterUserPassword" => "Automata", "NodeType" => "dc1_large"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
 
-      iex> ExAws.RedShift.create_cluster([ClusterIdentifier: "2B", MasterUsername: "Nier", MasterUserPassword: "Automata", NodeType: "dc1_large"])
+      iex> ExAws.RedShift.create_cluster([cluster_identifier: "2B", master_username: "Nier", master_user_password: "Automata", node_type: "dc1_large"])
       %ExAws.Operation.Query{action: "CreateCluster",
         params: %{"Action" => "CreateCluster", "ClusterIdentifier" => "2B", "MasterUsername" => "Nier", "MasterUserPassword" => "Automata", "NodeType" => "dc1_large"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
   """
-  @create_cluster_opts [:ClusterIdentifier, :MasterUsername, :MasterUserPassword,
-    :NodeType, :AllowVersionUpgrade, :AutomatedSnapshotRetentionPeriod, :AvailabilityZone,
-    :ClusterParameterGroupName, :ClusterSubnetGroupName, :ClusterType, :ClusterVersion,
-    :DBName, :ElasticIp, :Encrypted, :HsmClientCertificateIdentifier, :HsmConfigurationIdentifier,
-    :"IamRoles.IamRoleArn.N"]
+  @create_cluster_opts [:cluster_identifier, :master_username, :master_user_password,
+    :node_type, :allow_version_upgrade, :automated_snapshot_retention_period, :availability_zone,
+    :cluster_parameter_group_name, :cluster_subnet_group_name, :cluster_type, :cluster_version,
+    :db_name, :elastic_ip, :encrypted, :hsm_client_certificate_identifier, :hsm_configuration_identifier,
+    :"iam_roles/_iam_role_arn"]
   @spec create_cluster(Keyword.t) :: ExAws.Operation.Query.t
   @spec create_cluster(String.t, Keyword.t) :: ExAws.Operation.Query.t
   def create_cluster(opts) when is_list(opts) do
@@ -44,7 +46,7 @@ defmodule ExAws.RedShift do
   end
   def create_cluster(id, opts) when is_bitstring(id) do
     opts
-    |> set(:ClusterIdentifier, id)
+    |> set(:cluster_identifier, id)
     |> create_cluster()
   end
 
@@ -52,7 +54,7 @@ defmodule ExAws.RedShift do
   @doc """
   Delete a RedShift cluster with the a given identifier.
 
-  Required key is ClusterIdentifier.
+  Required key is cluster_identifier.
 
   Amazon docs: http://docs.aws.amazon.com/redshift/latest/APIReference/API_DeleteCluster.html
 
@@ -63,7 +65,8 @@ defmodule ExAws.RedShift do
         params: %{"Action" => "DeleteCluster", "ClusterIdentifier" => "2B"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
   """
-  @delete_cluster_opts [:ClusterIdentifier, :FinalClusterSnapshotIdentifier, :SkipFinalClusterSnapshot]
+  @type delete_cluster_opts :: []
+  @delete_cluster_opts [:cluster_identifier, :final_cluster_snapshot_identifier, :skip_final_cluster_snapshot]
   @spec delete_cluster(Keyword.t) :: ExAws.Operation.Query.t
   @spec delete_cluster(String.t, Keyword.t) :: ExAws.Operation.Query.t
   def delete_cluster(opts) when is_list(opts) do
@@ -73,7 +76,7 @@ defmodule ExAws.RedShift do
   end
   def delete_cluster(id, opts \\ []) when is_bitstring(id) do
     opts
-    |> set(:ClusterIdentifier, id)
+    |> set(:cluster_identifier, id)
     |> delete_cluster()
   end
 
@@ -86,12 +89,12 @@ defmodule ExAws.RedShift do
 
   ## Examples
 
-      iex> ExAws.RedShift.describe_clusters([ClusterIdentifier: "2B"])
+      iex> ExAws.RedShift.describe_clusters([cluster_identifier: "2B"])
       %ExAws.Operation.Query{action: "DescribeClusters",
         params: %{"Action" => "DescribeClusters", "ClusterIdentifier" => "2B"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
 
-      iex> ExAws.RedShift.describe_clusters([ClusterIdentifier: "2B", some_wrong_key: "Eve"])
+      iex> ExAws.RedShift.describe_clusters([cluster_identifier: "2B", some_wrong_key: "Eve"])
       %ExAws.Operation.Query{action: "DescribeClusters",
         params: %{"Action" => "DescribeClusters", "ClusterIdentifier" => "2B"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
@@ -101,12 +104,13 @@ defmodule ExAws.RedShift do
         params: %{"Action" => "DescribeClusters", "ClusterIdentifier" => "A2"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
 
-      iex> ExAws.RedShift.describe_clusters("9S", [ClusterIdentifier: "21O"])
+      iex> ExAws.RedShift.describe_clusters("9S", [cluster_identifier: "21O"])
       %ExAws.Operation.Query{action: "DescribeClusters",
         params: %{"Action" => "DescribeClusters", "ClusterIdentifier" => "9S"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
   """
-  @describe_clusters_opts [:ClusterIdentifier, :Marker, :MaxRecords, :"TagKeys.TagKey.N", :"TagValues.TagValue.N"]
+  @describe_clusters_opts [:cluster_identifier, :marker, :max_records, :"tag_keys/_tag_key",
+    :"tag_values/_tag_value"]
   @spec describe_clusters(Keyword.t) :: ExAws.Operation.Query.t
   @spec describe_clusters(String.t, Keyword.t) :: ExAws.Operation.Query.t
   def describe_clusters(opts \\ []) when is_list(opts) do
@@ -116,7 +120,7 @@ defmodule ExAws.RedShift do
   end
   def describe_clusters(id, opts) when is_bitstring(id) do
     opts
-    |> set(:ClusterIdentifier, id)
+    |> set(:cluster_identifier, id)
     |> describe_clusters()
   end
 
@@ -124,7 +128,7 @@ defmodule ExAws.RedShift do
   @doc """
   Modify a cluster with a given ClusterIdentifier.
 
-  Required key is ClusterIdentifier.
+  Required key is cluster_identifier.
 
   Amazon docs: http://docs.aws.amazon.com/redshift/latest/APIReference/API_ModifyCluster.html
 
@@ -135,34 +139,66 @@ defmodule ExAws.RedShift do
         params: %{"Action" => "ModifyCluster", "ClusterIdentifier" => "Adam"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
 
-      iex> ExAws.RedShift.modify_cluster("Adam", [NewClusterIdentifier: "Eve"])
+      iex> ExAws.RedShift.modify_cluster("Adam", [new_cluster_identifier: "Eve"])
       %ExAws.Operation.Query{action: "ModifyCluster",
         params: %{"Action" => "ModifyCluster", "ClusterIdentifier" => "Adam", "NewClusterIdentifier" => "Eve"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
 
-      iex> ExAws.RedShift.modify_cluster("Adam", [ClusterIdentifier: "Eve"])
+      iex> ExAws.RedShift.modify_cluster("Adam", [cluster_identifier: "Eve", "cluster_security_groups/_cluster_security_group_name": ["test", "tast"]])
       %ExAws.Operation.Query{action: "ModifyCluster",
-        params: %{"Action" => "ModifyCluster", "ClusterIdentifier" => "Adam"},
+        params: %{"Action" => "ModifyCluster", "ClusterIdentifier" => "Adam", "ClusterSecurityGroups.ClusterSecurityGroupName.1" => "test", "ClusterSecurityGroups.ClusterSecurityGroupName.2" => "tast"},
         parser: &ExAws.Utils.identity/2, path: "/", service: "redshift"}
   """
-  @modify_cluster_opts [:ClusterIdentifier, :AdditionalInfo, :AllowVersionUpgrade,
-    :AutomatedSnapshotRetentionPeriod, :ClusterParameterGroupName, :"ClusterSecurityGroups.ClusterSecurityGroupName.N",
-    :ClusterType, :ClusterVersion, :ElasticIp, :EnhancedVpcRouting, :HsmClientCertificateIdentifier,
-    :HsmConfigurationIdentifier, :MasterUserPassword, :NewClusterIdentifier,
-    :NodeType, :NumberOfNodes, :PreferredMaintenanceWindow, :PubliclyAccessible,
-    :"VpcSecurityGroupIds.VpcSecurityGroupId.N"]
+  @modify_cluster_opts [:cluster_identifier, :additional_info, :allow_version_upgrade,
+    :automated_snapshot_retention_period, :cluster_parameter_group_name, :"cluster_security_groups/_cluster_security_group_name",
+    :cluster_type, :cluster_version, :elastic_ip, :enhanced_vpc_routing, :hsm_client_certificate_identifier,
+    :hsm_onfiguration_identifier, :master_user_password, :new_cluster_identifier,
+    :node_type, :number_of_nodes, :preferred_maintenance_window, :publicly_accessible,
+    :"vpc_security_group_ids/_vpc_security_group_id"]
   @spec modify_cluster(Keyword.t) :: ExAws.Operation.Query.t
   @spec modify_cluster(String.t, Keyword.t) :: ExAws.Operation.Query.t
-  def modify_cluster(opts) do
+  def modify_cluster(opts) when is_list(opts) do
     opts
     |> build_params(@modify_cluster_opts)
     |> build_request("ModifyCluster")
   end
-  def modify_cluster(id, opts \\ []) do
+  def modify_cluster(id, opts \\ []) when is_bitstring(id) do
     opts
-    |> set(:ClusterIdentifier, id)
+    |> set(:cluster_identifier, id)
     |> modify_cluster()
   end
+
+
+  @doc """
+  A function for expanding a list of values for enumerable keys.
+
+  Some AWS keys support multiple values like roles, security groups, or tags.
+  The key usually follows the format `Names.Name.<index>`, for example,
+  `IamRoles.IamRoleArn.1` as seen in the examples below. The function takes
+  the key and a list of values, then append numerical index to the key using
+  the given separator and pair it with each value.
+
+  ## Examples
+
+      iex> ExAws.RedShift.expand_params({"iam_roles/_iam_role_arn", ["Operator", "Scanner"]})
+      [{"iam_roles/_iam_role_arn/1", "Operator"}, {"iam_roles/_iam_role_arn/2", "Scanner"}]
+
+      iex> ExAws.RedShift.expand_params({"IamRoles.IamRoleArn", ["Operator", "Scanner"]}, ".")
+      [{"IamRoles.IamRoleArn.1", "Operator"}, {"IamRoles.IamRoleArn.2", "Scanner"}]
+
+      iex> ExAws.RedShift.expand_params({"iam_roles/_iam_role_arn", []})
+      []
+  """
+  def expand_params(item, separator \\ "/")
+  def expand_params({k, v}, separator) when is_list(v) do
+    {params, _} = v
+    |> Enum.reduce({[], 1}, fn(item, {acc, index}) ->
+      {[{"#{k}#{separator}#{index}", item} | acc], index + 1}
+    end)
+    params
+    |> Enum.reverse
+  end
+  def expand_params(item, _), do: [item]
 
   # This function is used for setting the key value pair into the option list.
   # It inherits the functionality of `Keyword.update/4` meaning if there's any
@@ -184,8 +220,15 @@ defmodule ExAws.RedShift do
     |> Enum.filter(fn({k, _}) ->
       Enum.member?(keys, k)
     end)
+    |> Enum.reduce([], fn({k, v}, acc) ->
+      params = {k, v}
+      |> expand_params
+
+      Enum.concat(acc, params)
+    end)
     |> Enum.reduce(%{}, fn({k, v}, acc) ->
-      acc |> Map.put(to_string(k), v)
+      param = k |> to_string() |> camelize_key()
+      acc |> Map.put(param, v)
     end)
   end
 
